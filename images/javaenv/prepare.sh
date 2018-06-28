928ed2d9028b46dfc4771fca337d470c859ab677
Dumi Loghin <dumiloghin@gmail.com>
2018-06-28 10:00:13 +0000

    javaenv Dockerfile for armv7l

--- images/javaenv/prepare.sh
+++ images/javaenv/prepare.sh
@@ -3,14 +3,23 @@
 # Prepare build environment for different architectures,
 # such as arm64(x86_64) and aarch64(arm64)
 #
-# (c) 2018 - Dumi Loghin (dumiloghin@gmail.com)
+# (c) 2018 - Dumi Loghin
 #
 ARCH=`uname -m`
 CDIR=`pwd`
 SDIR=`dirname $0`
 # go to images/javaenv
 cd $SDIR
-if [ "$ARCH" == "aarch64" ]; then
+if [ "$ARCH" == "armv7l" ]; then
+	cd ../../
+	cp precompiled/armv7l/* build/image/javaenv/
+	cd images/javaenv
+	rm -f Dockerfile.in
+	cp Dockerfile.in.armv7l Dockerfile.in
+	cd ../../core/chaincode/shim/java
+	rm -f build.gradle
+	cp build.gradle.aarch64 build.gradle
+elif [ "$ARCH" == "aarch64" ]; then
 	# go to parent
 	cd ../../
 	cp precompiled/aarch64/* build/image/javaenv/
